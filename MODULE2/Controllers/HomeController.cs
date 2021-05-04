c8398de9ac0cea55cbdb260468ac879802160e35
Danny Garcia <dgarcia7303@southwestuniversity.edu>
2021-05-04 04:32:50 -0600

    MODULE 9
    ADDED SEARCH BAR
    AND STYLE PAGE AND STYLED WHAT ASKED FOR.

--- MODULE2/Controllers/HomeController.cs
+++ MODULE2/Controllers/HomeController.cs
@@ -45,6 +45,10 @@ namespace MODULE2.Controllers
         {
             return View();
         }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
